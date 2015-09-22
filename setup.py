4d1cd9e2046e0b621446e8250cdf492b5db5487b
Sun <orcfeng@gmail.com>
2015-09-22 17:55:02 +0000

    init

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Django On OpenShift with Django 1.8',
+setup(name='Bluepage',
     version='0.1',
-    description='django on OpenShift',
-    author='Gregory Pacheco',
-    author_email='contato@gregorypacheco.com.br',
-    url='https://github.com/Gpzim98/Django-OpenShift-1.8.git',
+    description='Consulting HUB',
+    author='Sun',
+    author_email='sun@bluepage.me',
+    url='ssh://560187680c1e66b1170000d9@bluepage-bluepage.rhcloud.com/~/git/bluepage.git/',
     install_requires=['Django==1.8.2',],
 )
