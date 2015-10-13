0f0ee56ff5817458f8f0a2c7c7adb2102c078ec7
Sun <orcfeng@gmail.com>
2015-10-13 23:51:50 +0000

    enable static

--- project/urls.py
+++ project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', 'project.views.home', name='home'),
@@ -23,4 +25,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^ckeditor/', include('ckeditor.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
