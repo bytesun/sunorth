61cce2810f366a06d6a1b0f07277df1b7c6dabc8
Sun <orcfeng@gmail.com>
2016-10-25 04:19:32 +0000

    add locale middleware

--- project/settings.py
+++ project/settings.py
@@ -76,7 +76,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-
+    'django.middleware.locale.LocaleMiddleware',
 
 )
 
