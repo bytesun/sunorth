088e92f5108915d5a9274d9927dc0f91208fbc38
Sun <orcfeng@gmail.com>
2017-09-10 21:24:27 +0000

    default mysql

--- project/settings.py
+++ project/settings.py
@@ -104,29 +104,29 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-if ON_PAAS:
+# if ON_PAAS:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME':     os.environ['OPENSHIFT_APP_NAME'],
-            'USER':     os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
-            'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
-            'HOST':     os.environ['OPENSHIFT_MYSQL_DB_HOST'],
-            'PORT':     os.environ['OPENSHIFT_MYSQL_DB_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':     os.environ['OPENSHIFT_APP_NAME'],
+        'USER':     os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
+        'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
+        'HOST':     os.environ['OPENSHIFT_MYSQL_DB_HOST'],
+        'PORT':     os.environ['OPENSHIFT_MYSQL_DB_PORT'],
     }
-    
+}
+
 
         
-else:
-    # stock django, local development.
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# else:
+#     # stock django, local development.
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
 # Internationalization
